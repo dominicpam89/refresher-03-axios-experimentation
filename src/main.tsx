bdcc9e69c6ea975cfa84fd946e05f37f007c78dd
dominicpam89 <dominicpam89@gmail.com>
2026-04-16 10:56:22 +0700

    establish tanstack query provider in the project: /lib/queryCLientProvider.ts, package.json, package-lock.json, main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import QueryClientWrapper from '@/lib/query-client-provider.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientWrapper>
+      <App />
+    </QueryClientWrapper>
   </StrictMode>
 );
