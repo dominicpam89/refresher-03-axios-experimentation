0e2c2ef371933bdc3d47ef154317c8696a40197c
dominicpam89 <dominicpam89@gmail.com>
2026-04-16 12:10:47 +0700

    update src/features/product/hook.ts

--- src/features/product/hook.ts
+++ src/features/product/hook.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { queryKey } from '@/features/product/query-key';
 import { getProducts } from '@/features/product/api';
 
-export const useProducts = () => {
+export const useGetProducts = () => {
   return useQuery({
     queryKey: queryKey.list(),
     queryFn: getProducts,
