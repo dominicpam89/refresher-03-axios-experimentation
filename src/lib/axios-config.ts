ff6c4affa618ae5fbe3adfa0fab0710974bd2b13
dominicpam89 <dominicpam89@gmail.com>
2026-04-16 11:08:12 +0700

    establish axios-config.ts

---
+++ src/lib/axios-config.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const apiProduct = axios.create({
+  baseURL: 'https://fakestoreapi.com',
+  timeout: 10000,
+});
+
+export const apiPost = axios.create({
+  baseURL: 'https://dummyjson.com',
+  timeout: 10000,
+});
