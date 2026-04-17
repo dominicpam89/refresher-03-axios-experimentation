a10f7cb46673832a344ab6170db7e931a5f93aed
dominicpam89 <dominicpam89@gmail.com>
2026-04-17 09:50:12 +0700

    add apiUser

--- src/lib/axios-config.ts
+++ src/lib/axios-config.ts
@@ -9,3 +9,8 @@ export const apiPost = axios.create({
   baseURL: 'https://dummyjson.com',
   timeout: 10000,
 });
+
+export const apiUser = axios.create({
+  baseURL: 'https://dummyjson.com',
+  timeout: 10000,
+});
