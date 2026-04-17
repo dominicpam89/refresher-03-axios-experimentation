ae22f6d6addf7058c2ebccbafb4d9ac69c6ae775
dominicpam89 <dominicpam89@gmail.com>
2026-04-17 10:34:02 +0700

    establish blog-type.ts for Post Type

---
+++ src/types/blog-type.ts
@@ -0,0 +1,14 @@
+export type Post = {
+  id: number;
+  title: string;
+  body: string;
+  tags: string[];
+  reactions: {
+    likes: number;
+    dislikes: number;
+  };
+  views: number;
+  userId: number;
+};
+
+export type PostCreate = Pick<Post, 'title' | 'body' | 'tags'>;
