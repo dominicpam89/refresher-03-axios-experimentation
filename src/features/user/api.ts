a0c91203f182fde35ade05a4c3c495cdb55d76f8
dominicpam89 <dominicpam89@gmail.com>
2026-04-17 10:06:17 +0700

    update and repair src/features/user/api.ts

--- src/features/user/api.ts
+++ src/features/user/api.ts
@@ -3,6 +3,6 @@ import type { AxiosResponse } from 'axios';
 import type { UserType } from '@/type';
 
 export const getUsers = async function () {
-  const { data }: AxiosResponse<UserType> = await apiUser.get('/users');
+  const { data }: AxiosResponse<UserType[]> = await apiUser.get('/users');
   return data;
 };
