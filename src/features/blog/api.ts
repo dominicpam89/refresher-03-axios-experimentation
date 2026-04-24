78dacbd85ef7d5166abb72709f2218fd308f81e3
dominicpam89 <dominicpam89@gmail.com>
2026-04-24 14:27:52 +0700

    establish api create post and its tanstack (useMutation)

--- src/features/blog/api.ts
+++ src/features/blog/api.ts
@@ -1,8 +1,26 @@
 import { apiPost } from '@/lib/axios-config';
-import type { PostResponseType } from '@/type';
-import type { AxiosResponse } from 'axios';
+import type { PostCreateResponseType, PostCreateType, PostsResponseType } from '@/type';
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export const getPosts = async function () {
-  const { data }: AxiosResponse<PostResponseType> = await apiPost.get('/posts');
+  const { data }: AxiosResponse<PostsResponseType> = await apiPost.get('/posts');
   return data;
+};
+
+export const createPost = async function (newPost: PostCreateType) {
+  const requestConfig: AxiosRequestConfig<PostCreateType> = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const response: AxiosResponse<PostCreateResponseType> = await apiPost.post(
+    '/posts/add',
+    {
+      ...newPost,
+      userId: 5,
+    },
+    requestConfig
+  );
+  return response.data;
 };
--- src/features/blog/hooks/query-hooks.ts
+++ src/features/blog/hooks/query-hooks.ts
@@ -1,10 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { queryKeys } from '@/features/blog/query-key';
-import { getPosts } from '@/features/blog/api';
+import { getPosts, createPost } from '@/features/blog/api';
+import { queryClient } from '@/lib/query-client-provider';
 
 export const useGetPosts = () => {
   return useQuery({
     queryKey: queryKeys.list(),
     queryFn: getPosts,
+  });
+};
+
+export const useCreatePost = () => {
+  return useMutation({
+    mutationFn: createPost,
+    onError(error, variables, onMutateResult, context) {
+      console.error('fail to create post', error);
+      console.log('variables: ', variables);
+      console.log('onMutateResult: ', onMutateResult);
+      console.log('context: ', context);
+    },
+    onMutate(variables, context) {
+      console.log('creating post...');
+      console.log('variables: ', variables);
+      console.log('context: ', context);
+    },
+    onSuccess: (data, variables, onMutateResult, context) => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.list() });
+      console.log('data: ', data);
+      console.log('variables: ', variables);
+      console.log('onMutateResult: ', onMutateResult);
+      console.log('context: ', context);
+    },
   });
 };
