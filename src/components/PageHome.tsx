4b99343eb0187bc83342027900c18cf40cc34d83
dominicpam89 <dominicpam89@gmail.com>
2026-05-26 12:04:52 +0700

    - establish PageHome.tsx in ./src/components/ - establish index.tsx in ./src/routes/

---
+++ src/components/PageHome.tsx
@@ -0,0 +1,13 @@
+export default function PageHome() {
+  return (
+    <div className="mx-auto mt-16 max-w-lg min-w-xs">
+      <h1>Page Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+        necessitatibus. Consectetur ullam, obcaecati, non dolorum magnam
+        architecto dolores totam consequuntur quam suscipit provident vel libero
+        animi nihil, illum labore aut?
+      </p>
+    </div>
+  );
+}
