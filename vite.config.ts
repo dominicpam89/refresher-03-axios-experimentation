e69316b2bbc7038bccf3ab62ecf4787785811022
dominicpam89 <dominicpam89@gmail.com>
2026-05-26 11:54:12 +0700

    - install @tanstack/router-plugin - update vite.config.js to use tanstackRouter plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tanstackRouter(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
   resolve: {
