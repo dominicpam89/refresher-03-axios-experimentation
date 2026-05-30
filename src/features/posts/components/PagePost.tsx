18ed3bbbf6c8eed64a9b3d4727f1bf9b71c5a16f
dominicpam89 <dominicpam89@gmail.com>
2026-05-30 14:48:09 +0700

    add search param functionality in PagePost in ./src/features/posts/components/PagePost.tsx

--- src/features/posts/components/PagePost.tsx
+++ src/features/posts/components/PagePost.tsx
@@ -1,13 +1,28 @@
 import { useLoaderData, useNavigate } from '@tanstack/react-router';
 import PostOverview from './PostOverview';
+import PageNavigation from './PageNavigation';
 
 export default function PagePost() {
   const { posts, page, hasMore } = useLoaderData({
     from: '/posts',
   });
+
+  const navigate = useNavigate();
+
+  const goToPage = (newPage: number) => {
+    navigate({
+      to: '/posts',
+      search: { page: newPage },
+      replace: true,
+    });
+  };
+
+  const navigationProps = { page, hasMore, goToPage };
+
   return (
     <section id="page-posts" className="flex flex-col gap-4">
       <h1>Paginated Posts Page</h1>
+      <PageNavigation {...navigationProps} />
       {posts.map((post) => (
         <PostOverview key={post.id} post={post} />
       ))}
