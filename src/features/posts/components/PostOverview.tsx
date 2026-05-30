00adeb4e8a98db7c8b85594ff672516e76ea14a5
dominicpam89 <dominicpam89@gmail.com>
2026-05-30 14:21:13 +0700

    establish PostOverview component in ./src/features/posts/components/

---
+++ src/features/posts/components/PostOverview.tsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import type { Post } from '@/features/posts/types/post.type';
+import { Link } from '@tanstack/react-router';
+
+interface Props {
+  post: Post;
+}
+
+export default function PostOverview({ post }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{post.title}</CardTitle>
+        <CardDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea neque eius
+          blanditiis laboriosam, amet, sapiente sint odit doloremque ex quae
+          necessitatibus nobis. Ipsa id rem aliquid, a saepe aliquam atque?
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {post.body.substring(0, 100)}...
+        <Button asChild variant="link">
+          <Link to={`/posts/${post.id}`}>Read More</Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
