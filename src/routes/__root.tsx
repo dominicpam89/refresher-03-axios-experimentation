9f45c67da57f1e54635e6bbabd67d0ee648079e3
dominicpam89 <dominicpam89@gmail.com>
2026-06-03 15:33:05 +0700

    update root route to use RouterContext

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
-import { createRootRoute } from '@tanstack/react-router';
+import { createRootRouteWithContext } from '@tanstack/react-router';
+import { type RouterContext } from '@/types/router-context.type';
 import RootLayout from '@/components/RootLayout';
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootLayout,
 });
