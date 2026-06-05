fd25e41fb7fbd34ff95a9b4c4139753e1f689cde
dominicpam89 <dominicpam89@gmail.com>
2026-06-05 11:07:15 +0700

    establish mock api

---
+++ src/lib/api.mock.ts
@@ -0,0 +1,39 @@
+type LoginParam = {
+  email: string;
+  password: string;
+  mockTime?: number;
+};
+
+type ProtectedDataParam = {
+  mockTime?: number;
+};
+
+type LoginPromise = { token: string };
+type ProtectedDataPromise = { data: string };
+
+const mockLogin = ({ email, password, mockTime = 500 }: LoginParam) => {
+  return new Promise<LoginPromise>((resolve, reject) => {
+    setTimeout(() => {
+      if (email === 'user@example.com' && password === 'pass')
+        resolve({ token: 'fake-jwt' });
+      else {
+        reject({
+          response: { status: 401, data: { message: 'Invalid Credentials' } },
+        });
+      }
+    }, mockTime);
+  });
+};
+
+const mockProtectedData = ({ mockTime = 500 }: ProtectedDataParam) => {
+  return new Promise<ProtectedDataPromise>((resolve, reject) => {
+    setTimeout(() => {
+      const token = localStorage.getItem('token');
+      token === 'fake-jwt'
+        ? resolve({ data: 'Secret Dashboard Data' })
+        : reject({ response: { status: 401 } });
+    }, mockTime);
+  });
+};
+
+export { mockLogin, mockProtectedData };
